8cbfec27e5cb2fa66d9dfd78e1aea4018bc74420
l-gonz <l.gonzalezfernan@gmail.com>
2019-08-17 22:42:10 +0100

    Add behaviour to roll button
    The button on the MainActivity.kt file can be clicked to call rollDie().

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.helloworld
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton : Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener( rollDie() )
+    }
+
+    private fun rollDie() {
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
 }
