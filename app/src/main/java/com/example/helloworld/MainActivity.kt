3da3d64aa5e0f41c52b3d824f1b9f362bac8eb24
l-gonz <l.gonzalezfernan@gmail.com>
2019-08-18 22:36:35 +0100

    Fix lambda expression on button listener
    Parenthesis around the lambda expression on 'setOnClickListener' made
    Kotlin go crazy. Who new?

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton : Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener( rollDie() )
+        rollButton.setOnClickListener { rollDie() }
     }
 
     private fun rollDie() {
