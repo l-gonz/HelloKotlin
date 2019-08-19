c3f60d05a704e512e57d936821f776dfbd49b064
l-gonz <l.gonzalezfernan@gmail.com>
2019-08-19 22:23:45 +0100

    Finish feature with coding challenge
    A die is rolled with a button
    The count can be manually increased with another button

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.helloworld
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val MAX_COUNT = 6
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,9 +18,31 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton : Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDie() }
+
+        val countUpButton : Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { countUp() }
     }
 
-    private fun rollDie() {
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+    private fun rollDie()
+    {
+        count = Random().nextInt(MAX_COUNT) + 1
+
+        val resultText : TextView = findViewById(R.id.result_text)
+        resultText.text = "Number is $count"
+        Toast.makeText(this, (R.string.result_random), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun countUp()
+    {
+        if (count == MAX_COUNT)
+        {
+            Toast.makeText(this, (R.string.result_max), Toast.LENGTH_SHORT).show()
+            return
+        }
+        count++
+
+        val resultText : TextView = findViewById(R.id.result_text)
+        resultText.text = "Number is $count"
+        Toast.makeText(this, (R.string.result_up), Toast.LENGTH_SHORT).show()
     }
 }
